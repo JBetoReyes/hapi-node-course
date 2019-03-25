9427d63571718d16fc20eab99873f28aa82de609
JBetoReyes <josereyes55@hotmail.com>
2019-03-25 17:24:59 -0600

    creates the exercises module

---
+++ src/app/pages/exercises/exercises.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HapiExercisesComponent } from './hapi-exercises/hapi-exercises.component';
+
+@NgModule({
+  declarations: [HapiExercisesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExercisesModule { }
---
+++ src/app/pages/exercises/hapi-exercises/hapi-exercises.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hapi-exercises',
+  templateUrl: './hapi-exercises.component.html',
+  styleUrls: ['./hapi-exercises.component.scss']
+})
+export class HapiExercisesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
