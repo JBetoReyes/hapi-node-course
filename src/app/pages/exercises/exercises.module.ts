24477d9952b7bc10df3cb1ce271aa3e4b468bd66
JBetoReyes <josereyes55@hotmail.com>
2019-03-30 13:12:20 -0600

    adding format to the exercises

--- src/app/pages/exercises/exercises.module.ts
+++ src/app/pages/exercises/exercises.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HapiExercisesComponent } from './hapi-exercises/hapi-exercises.component';
 import { Exercise01Component } from './hapi-exercises/presentation/exercise01/exercise01.component';
+import {MatExpansionModule} from '@angular/material';
+import {MarkdownModule} from 'ngx-markdown';
+import { Exercise02Component } from './hapi-exercises/presentation/exercise02/exercise02.component';
 
 @NgModule({
-  declarations: [HapiExercisesComponent, Exercise01Component],
+  declarations: [HapiExercisesComponent, Exercise01Component, Exercise02Component],
   imports: [
-    CommonModule
+    CommonModule,
+    MatExpansionModule,
+    MarkdownModule.forChild()
   ],
   exports: [
     HapiExercisesComponent
---
+++ src/app/pages/exercises/hapi-exercises/presentation/exercise02/exercise02.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercise02',
+  templateUrl: './exercise02.component.html',
+  styleUrls: ['./exercise02.component.scss']
+})
+export class Exercise02Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
